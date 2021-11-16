bd008ce6584b015c6aae26919fc55709885c9a13
Luca <90553744+digirolamoluca@users.noreply.github.com>
2021-11-16 16:29:25 +0100

    Update generator_cypher.py

--- generator_cypher.py
+++ generator_cypher.py
@@ -4,19 +4,17 @@ from config import *
 from macm import *
 
 
-
 node_container = []
 node_networks = []
 edge_uses = []
 edge_connects = []
-
+edge_connects_default_networks = []
+edge_hosts = []
 
 container_name = []
 
-
 networks = []
 networks_connects = []
-
 
 depends_on = []
 
@@ -30,10 +28,8 @@ fp2=fp.read()
 
 
 
-
 #converto yml in dizionario:
 docker_composeObj=(yaml.safe_load(fp2))
-
 
 
 
@@ -41,7 +37,6 @@ docker_composeObj=(yaml.safe_load(fp2))
 for key,value in docker_composeObj['services'].items():
 	container_name.append(key)
 
-	
 
 
 #NETWORKS: ottengo le networks associate ai singoli container per le relazioni di uses	
@@ -53,17 +48,15 @@ for x in container_name:
 			ind+=1
 			networks_connects.append(docker_composeObj['services'][x]['networks'])
 if ind==0:
-	print("Non sono presenti 'networks' nel docker compose file\n")
+	print("Non è presente alcuna 'networks' nel docker compose\n")
 
-	
 
 
 #NETWORKS: ottengo le networks(uno per tipo non replicato) per la creazione di nodi networks(se presenti)
 if(len(networks_connects)!=0):
 	for key in docker_composeObj['networks'].keys():
 		networks.append(key)
-
-		
+	
 	
 	
 #DEPENDS_ON: ottengo le depends_on associate ai singoli container per le relazioni di connects
@@ -75,21 +68,37 @@ for x in container_name:
 			ind+=1
 			depends_on.append(docker_composeObj['services'][x]['depends_on'])
 if ind==0:
-	print("Non sono presenti 'depends_on' nel docker compose file\n")	
+	print("Non è presente nessuna 'depends_on' nel docker compose\n")	
 
 
 
+#creo nodo VM che fa da host per tutti i container
+VM=Node("IaaS","VM","TBD",app_id,application_name)
+VM.addnode()
 
+
+
+#se c'è qualche container privo di networks creo nodo default-network
+if(len(networks_connects) != len(container_name)):
+	default_network=Node("network","default-network","TBD",app_id,application_name)
+	default_network.addnode()
+
+	
 
 #creato i nodi container
 ind=0
 for x in container_name:
 	node_container.append(Node("service",x,"TDB",app_id,application_name))
 	node_container[ind].addnode()
-	ind+=1
+    #file_cypher.write("\t("+container_name_filter[ind]+":service {name:'"+x+"', type:'TDB', app_id:'"+app_id+"', application:'"+application_name+"'}),\n")
+	ind+=1   
 
 	
-
+	
+#per pulizia di codice in output:
+file_cypher.write("\n")	
+	
+	
 	
 #creo i nodi networks(se presenti)
 if(len(networks_connects)!=0):
@@ -97,14 +106,13 @@ if(len(networks_connects)!=0):
 	for x in networks:
 		node_networks.append(Node("network",x,"TDB",app_id,application_name))
 		node_networks[ind].addnode()
+		#file_cypher.write("\t("+networks_filter[ind]+":service {name:'"+x+"', type:'TDB', app_id:'"+app_id+"', application:'"+application_name+"'}),\n")
 		ind+=1  
 
 
 
-		
 #per pulizia di codice in output:
 file_cypher.write("\n")
-
 
 
 
@@ -138,13 +146,43 @@ if(len(depends_on)!=0):
 
 
 
-		
 #per pulizia di codice in output:
 file_cypher.write("\n")
 
 
 
+#creo gli archi connects per connettere alla default-network quando il container non ha una network associata
+properties = []
+ind=0
+net = []
+for x in container_name:
+	properties.append(list(docker_composeObj['services'][x].keys()))
 
+	
+for j in range(0,len(container_name),1):
+	ind=0	
+	for i in range(0,len(properties[j]),1):
+		if(properties[j][i]=='networks'):
+			net.insert(j,1)
+		if(properties[j][i]!='networks'):
+			ind+=1
+		if(ind==len(properties[j])):
+			net.insert(j,0)
+
+count=0	
+for j in range(0,len(net),1):
+	if(net[j]==0):
+		edge_connects_default_networks.append(Edge(node_container[j],default_network,"connects"))
+		edge_connects_default_networks[count].addedge(0)
+		count+=1
+
+
+		
+#per pulizia di codice in output:
+file_cypher.write("\n")
+
+
+		
 #creo gli archi connects se le networks sono presenti nel docker compose
 #il valore q lo utilizziamo per non inserire la virgola nell'ultima connect
 if(len(networks_connects)!=0):
@@ -154,7 +192,7 @@ if(len(networks_connects)!=0):
 	for x in container_name:
 		for key in docker_composeObj['services'][x].keys():
 			if(key=='networks'):
-				q=len(container_name)-1
+				#q=len(container_name)-1
 				for i in range(0,len(networks_connects[ind]),1):
 					ind2=0
 					to_node = None
@@ -164,20 +202,69 @@ if(len(networks_connects)!=0):
 					
 					#il nodo da cui parte l'arco è corretto grazie all'if che abbiamo inserito	
 					edge_connects.append(Edge(node_container[count],to_node,"connects"))
-					
-					#l'if seguente viene utilizzato per alzare l'end:flag (e quindi in sostanza non inserire la virgola nella parte finale della query)
-					if ind!=q: #se non sono a fine query
-						edge_connects[count_edge].addedge(0)
-						count_edge+=1
-					if ind==q: #se sono a fine query
-						edge_connects[count_edge].addedge(1)
-						count_edge+=1
+					edge_connects[count_edge].addedge(0)
+					count_edge+=1
 				ind+=1
 		count+=1		
 
 
-		
+
+#per pulizia di codice in output:
+file_cypher.write("\n")
+
+
+
+#creo gli archi hosts da VM per tutti i container
+count_edge=0
+ind=0
+q=len(container_name)-1	
+for i in range(0,len(container_name),1):
+	ind2=0
+	to_node = None
+	while to_node == None:
+		to_node=getnode_fromname(node_container[ind2],container_name[i])
+		ind2+=1
+	
+	edge_hosts.append(Edge(VM,to_node,"hosts"))
+	
+	#l'if seguente viene utilizzato per alzare l'end:flag (e quindi in sostanza non inserire la virgola nella parte finale della query)
+	if ind!=q:
+		edge_hosts[count_edge].addedge(0)
+		count_edge+=1
+	if ind==q:	
+		edge_hosts[count_edge].addedge(1)
+		count_edge+=1
+	
+	ind+=1
+	
+	
+	
+print("\n")
+
+
+
+#segnalo in output quali container non presentano le depends_on (nel caso in cui le presentano tutte non ha alcun effetto)
+ind=0
+net2 = []
+for x in container_name:
+	properties.append(list(docker_composeObj['services'][x].keys()))
+
+	
+for j in range(0,len(container_name),1):
+	ind=0	
+	for i in range(0,len(properties[j]),1):
+		if(properties[j][i]=='depends_on'):
+			net2.insert(j,1)
+		if(properties[j][i]!='depends_on'):
+			ind+=1
+		if(ind==len(properties[j])):
+			net2.insert(j,0)
+
+for j in range(0,len(net2),1):
+	if(net2[j]==0):
+		print("Il container "+container_name[j]+" non presenta depends_on")
+
 
 #stampa dell'oggetto grafo finale
-print("L'oggetto grafo finale è il seguente:")
+print("\nL'oggetto grafo finale è il seguente:")
 print(get_graph())
