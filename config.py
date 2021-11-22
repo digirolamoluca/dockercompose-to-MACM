8d020819cb3365d33aa339b9aedda3dcbbdaa589
Luca <90553744+digirolamoluca@users.noreply.github.com>
2021-11-22 13:21:34 +0100

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-#inserire nome del file docker-compose (senza estensione). Il file docker-compose dovrà trovarsi nella stessa directory in cui è presente il generator_cypher
-dockercompose_name = "docker-compose"
-
-#inserire path della directory in cui salvare il macm in output
-#example: destination_path = "C:/Users/lucad/Desktop"
-destination_path = "dockercompose-to-MACM/Result"
